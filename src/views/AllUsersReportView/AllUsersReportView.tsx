8d7d171d584f03822830f463573e55b928b7cdb6
Ryszard-Penkala <ryszard.penkala@gmail.com>
2022-09-26 13:16:42 +0200

    grouped commit

--- src/views/AllUsersReportView/AllUsersReportView.tsx
+++ src/views/AllUsersReportView/AllUsersReportView.tsx
@@ -1,10 +1,44 @@
 import styles from './AllUsersReportView.module.scss';
+import {useEffect, useState} from "react";
+import {Spinner} from "../../common/Spinner/Spinner";
+import {getAllUsersReportResponse} from 'types';
 
 export const AllUsersReportView = () => {
 
+    const [loadingData, setLoadingData] = useState<getAllUsersReportResponse | null>(null)
 
-    return(
-        <h1>AllUsersReportView</h1>
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const response = await fetch(`http://localhost:3001/time-report/all-users`, {
+                credentials: 'include',
+            });
+            const json = await response.json()
+            setLoadingData(json);
+        }
+
+        fetchData()
+            .catch(console.error);
+    }, []);
+
+
+    if (loadingData === null) {
+        return <Spinner/>
+    }
+
+    if (Number(loadingData) === 401) {
+        return (
+            <h1>You shall not pass</h1>
+        )
+    }
+
+
+    return (
+        <>
+            <h1 className={styles.headerOne}>AllUsersReportView</h1>
+            <p>{loadingData[2].taskDescription}</p>
+        </>
     )
 
 }
